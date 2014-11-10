97c1b8f03e7a0405da10823678fcf0989c734539
Vaidas Lungis <vaidas.lungis+github@gmail.com>
2014-11-10 12:56:17 +0200

    added more functions
    getConfig(), updateConfig(), uploadImage(), saveImageRemoteId(), isSetClientId()

--- Service.php
+++ Service.php
@@ -51,4 +51,59 @@ class Service implements InjectionAwareInterface
 
         return $this->di['db']->exec("DROP TABLE IF EXISTS `imgur`") > 0;
     }
+
+    public function getConfig()
+    {
+        $extensionService = $this->di['mod_service']('Extension');
+        return $extensionService->getConfig('mod_imgur');
+    }
+
+    public function updateConfig($data = array())
+    {
+        if (!$this->isSetClientId($data)){
+            throw new \Box_Exception('Client ID param is missing');
+        }
+
+        $extensionService = $this->di['mod_service']('Extension');
+        $data['ext'] = 'mod_imgur';
+        return $extensionService->setConfig($data);
+    }
+
+    public function uploadImage($data)
+    {
+        if (!isset($data['image'])){
+            throw new \Box_Exception('Image is missing');
+        }
+
+        $extensionService = $this->di['mod_service']('Extension');
+        $config = $extensionService->getConfig('mod_imgur');
+
+        if (!$this->isSetClientId($config)){
+            throw new \Box_Exception('Client ID param is missing');
+        }
+
+        $headers = sprintf('Authorization: Client-ID %s', $config['client_id']);
+        $params = array(
+            'type'  => 'base64',
+            'image' => base64_encode($this->di['tools']->file_get_contents($data['image'])),
+        );
+
+        $client = $this->di['guzzle_client'];
+        return $client->post('https://api.imgur.com/3/image', $headers, $params);
+    }
+
+    public function saveImageRemoteId($image_id)
+    {
+        $model = $this->di['db']->dispense('Imgur');
+        $model->url = sprintf('https://api.imgur.com/3/image/%d', $image_id);
+        $model->created_at = date('c');
+        $model->updated_at = date('c');
+        $id = $this->di['db']->store($model);
+        return $id;
+    }
+
+    public function isSetClientId($config = array())
+    {
+        return isset($config['client_id']) && strlen(trim($config['client_id'])) > 0;
+    }
 }
--- tests/ServiceTest.php
+++ tests/ServiceTest.php
@@ -55,6 +55,163 @@ class ServiceTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($restult);
     }
 
+    public function testgetConfig()
+    {
+        $extensionServiceMock = $this->getMockBuilder('\Box\Mod\Extension\Service')->getMock();
+        $extensionServiceMock->expects($this->atLeastOnce())
+            ->method('getConfig')
+            ->with('mod_imgur')
+            ->willReturn(array());
+
+        $di = new \Box_Di();
+        $di['mod_service'] = $di->protect( function ($extensionName) use ($extensionServiceMock) {
+            if ($extensionName == 'Extension'){
+                return $extensionServiceMock;
+            }
+        });
+
+        $this->service->setDi($di);
+        $result = $this->service->getConfig();
+        $this->assertInternalType('array', $result);
+        $this->assertEmpty($result);
+    }
+
+    public function testupdateConfig()
+    {
+        $extensionServiceMock = $this->getMockBuilder('\Box\Mod\Extension\Service')->getMock();
+        $extensionServiceMock->expects($this->atLeastOnce())
+            ->method('setConfig')
+            ->willReturn(true);
+
+        $di = new \Box_Di();
+        $di['mod_service'] = $di->protect( function ($extensionName) use ($extensionServiceMock) {
+            if ($extensionName == 'Extension'){
+                return $extensionServiceMock;
+            }
+        });
+
+        $params = array(
+            'client_id' => '1eeee1234',
+            'secrect' => 'code',
+        );
+
+        $this->service->setDi($di);
+        $result = $this->service->updateConfig($params);
+        $this->assertInternalType('bool', $result);
+        $this->assertTrue($result);
+    }
+
+    public function testupdateConfig_Client_ID_missing()
+    {
+        $params = array(
+            'secrect' => 'code',
+        );
+
+        $this->setExpectedException('\Box_Exception', 'Client ID param is missing');
+        $this->service->updateConfig($params);
+    }
+
+    public function testuploadImage()
+    {
+        $config = array(
+            'client_id' => '1eeee1234',
+            'secrect' => 'code',
+        );
+        $extensionServiceMock = $this->getMockBuilder('\Box\Mod\Extension\Service')->getMock();
+        $extensionServiceMock->expects($this->atLeastOnce())
+            ->method('getConfig')
+            ->with('mod_imgur')
+            ->willReturn($config);
+
+        $toolsMock = $this->getMockBuilder('\Box_Tools')->getMock();
+        $toolsMock->expects($this->atLeastOnce())
+            ->method('file_get_contents');
+
+        $guzzleClientMock = $this->getMockBuilder('\Guzzle\Http\Client')->disableOriginalConstructor()->getMock();
+        $guzzleClientMock->expects($this->atLeastOnce())
+            ->method('post')
+            ->with('https://api.imgur.com/3/image')
+            ->willReturn(1);
+
+
+        $di = new \Box_Di();
+        $di['mod_service'] = $di->protect( function ($extensionName) use ($extensionServiceMock) {
+            if ($extensionName == 'Extension'){
+                return $extensionServiceMock;
+            }
+        });
+        $di['tools'] = $toolsMock;
+        $di['guzzle_client'] = $guzzleClientMock;
+
+        $this->service->setDi($di);
+
+        $data = array('image' => 'path/to/image');
+        $this->service->uploadImage($data);
+    }
+
+    public function testuploadImage_Missing_image()
+    {
+        $config = array(
+            'client_id' => '1eeee1234',
+            'secrect' => 'code',
+        );
+
+        $this->setExpectedException('\Box_Exception', 'Image is missing');
+        $this->service->uploadImage(array());
+    }
+
+    public function testuploadImage_Missing_client_id()
+    {
+        $config = array();
+        $extensionServiceMock = $this->getMockBuilder('\Box\Mod\Extension\Service')->getMock();
+        $extensionServiceMock->expects($this->atLeastOnce())
+            ->method('getConfig')
+            ->with('mod_imgur')
+            ->willReturn($config);
+
+        $di = new \Box_Di();
+        $di['mod_service'] = $di->protect( function ($extensionName) use ($extensionServiceMock) {
+            if ($extensionName == 'Extension'){
+                return $extensionServiceMock;
+            }
+        });
+
+        $this->service->setDi($di);
+
+        $data = array('image' => 'path/to/image');
+        $this->setExpectedException('\Box_Exception', 'Client ID param is missing');
+        $this->service->uploadImage($data);
+    }
+
+    public function testsaveImageRemoteId()
+    {
+
+        $model = new \RedBean_SimpleModel();
+        $model->loadBean(new \RedBeanPHP\OODBBean());
+
+        $dbMock = $this->getMockBuilder('\Box_Database')->getMock();
+        $dbMock->expects($this->atLeastOnce())
+            ->method('dispense')
+            ->with('Imgur')
+            ->willReturn($model);
+
+        $new_id = 1;
+        $dbMock->expects($this->atLeastOnce())
+            ->method('store')
+            ->with($model)
+            ->willReturn($new_id);
+
+        $di = new \Box_Di();
+        $di['db'] = $dbMock;
+
+        $this->service->setDi($di);
+        $result = $this->service->saveImageRemoteId(1);
+        $this->assertInternalType('int', $result);
+        $this->assertEquals($new_id, $result);
+    }
+
+
+
 
 
 }
